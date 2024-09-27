c940034bfc2f84caae6b92394f8fdc1f76783b30
Melih Øzata <129662619+Melihoz98@users.noreply.github.com>
2024-09-27 10:50:56 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
 import Projects from './Components/Projects';
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,3 @@
-// Home.jsx
 import React from 'react';
 import './Home.css';
 import profileImage from '../assets/profile-img.png';
@@ -14,9 +13,15 @@ const Home = () => {
               <img src={profileImage} alt="Your Name" className="profile-image" />
             </div>
             <div className="contact-info">
-              <a href="https://www.linkedin.com/in/melih-%C3%B8zata-3676832ba/" target="_blank" rel="noopener noreferrer" className="contact-link">LinkedIn</a>
-              <a href="https://github.com/Melihoz98" target="_blank" rel="noopener noreferrer" className="contact-link">GitHub</a>
-              <a href="mailto:melihozata2@gmail.com" className="contact-link">Email</a>
+              <a href="https://www.linkedin.com/in/melih-%C3%B8zata-3676832ba/" target="_blank" rel="noopener noreferrer" className="contact-link">
+                <i className="fab fa-linkedin"></i> LinkedIn
+              </a>
+              <a href="https://github.com/Melihoz98" target="_blank" rel="noopener noreferrer" className="contact-link">
+                <i className="fab fa-github"></i> GitHub
+              </a>
+              <a href="mailto:melihozata2@gmail.com" className="contact-link">
+                <i className="fas fa-envelope"></i> Email
+              </a>
             </div>
           </div>
 
@@ -24,9 +29,11 @@ const Home = () => {
           <div className="col-lg-6 right-section">
             <div className="welcome-text">
               <h1>Welcome!</h1>
-              <h2>I am [Your Name]</h2>
-              <p>Datamatiker studerende</p>
-            </div>
+              <h2>I'm Melih Øzata</h2>
+              <h2>Currently looking for an internship</h2>
+              <p>
+    <i className="fas fa-computer"></i> AP Graduate in Computer Science - Student
+  </p>            </div>
           </div>
         </div>
 
